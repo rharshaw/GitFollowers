75a7c5d906aa498dc62313924acd6fb176a14e86
Rian <63377157+rharshaw@users.noreply.github.com>
2022-05-09 12:48:50 -0400

    Initial Commit

---
+++ GitFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitFollowers
+//
+//  Created by Rian Harshaw on 5/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
