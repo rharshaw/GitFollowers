de520ea202f75f38e1d74187cf08788793c0a7d6
Rian <63377157+rharshaw@users.noreply.github.com>
2022-05-15 16:56:38 -0400

    Cell creation

---
+++ GitFollowers/Custom Views/GitFollowerAvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  GitFollowerAvatarImageView.swift
+//  GitFollowers
+//
+//  Created by Rian on 5/14/22.
+//
+
+import UIKit
+
+class GitFollowerAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
