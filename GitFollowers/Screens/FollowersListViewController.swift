30005cf6b6838562bf9a60ca99a3a149b1ed110a
Rian <63377157+rharshaw@users.noreply.github.com>
2022-05-25 22:05:12 -0400

    Added empty state view

--- GitFollowers/Screens/FollowersListViewController.swift
+++ GitFollowers/Screens/FollowersListViewController.swift
@@ -48,13 +48,21 @@ class FollowersListViewController: UIViewController {
     
     
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollower(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             switch result {
                 
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them. 😃"
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                    return
+                }
                 self.updateData()
                 
             case .failure(let error):
