701ed09893c6be746e12be046e6c6ae93b926195
Rian <63377157+rharshaw@users.noreply.github.com>
2022-06-12 18:13:43 -0400

    Forgot some files

---
+++ GitFollowers/Custom Views/ViewControllers/Item Info/FollowerItemViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FollowerItemViewController.swift
+//  GitFollowers
+//
+//  Created by Rian Harshaw on 6/11/22.
+//
+
+import Foundation
+import UIKit
+
+class FollowerItemViewController: ItemInfoViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Git Followers")
+    }
+}
